7c3963872f51b1b6b76d5039348706910aed5d47
Jordan Jenkins <JJenkins@ucvts.tec.nj.us>
2019-11-14 10:46:07 -0500

    i did nothinggg

--- src/sum.js
+++ src/sum.js
@@ -1,4 +1,17 @@
-const readlineSync = require("readline-sync")
+ const readlineSync = require("readline-sync")
 
 const MIN = Number.MIN_SAFE_INTEGER
 const MAX = Number.MAX_SAFE_INTEGER
+let sum = 0;
+
+const lowerBound = Number(readlineSync.question("\nLower bound: "));
+const upperBound = Number(readlineSync.question("Upper bound: "));
+
+while (lowerBound <= Number.MIN_SAFE_INTEGER || upperBound >= Number.MAX_SAFE_INTEGER) {
+  console.log("Upper bound: ");
+  console.log("Lower bound:");
+}
+ if (lowerBound % 2===0 && lowerbound !==0) {
+   sum = sum + lowerBound;
+ }
+ else if (lowerBound % 2!==0)
