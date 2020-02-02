1bd0312257f1e360acc8d1f0afbf98a001ae00b1
jordan-j23 <jordanejenk@gmail.com>
2020-02-02 08:58:20 -0500

    Update factors.js

--- src/factors.js
+++ src/factors.js
@@ -1,1 +1,31 @@
 const readlineSync = require("readline-sync");
+let value = 0;
+
+while (value <= 0) {
+value = readlineSync.question("Positive integer: ");
+num = parseInt(value);
+
+limit = Math.sqrt(num);
+
+
+factors = '';
+
+for (var i = 1; i <= limit; i++) { 
+if (num % i == 0) { 
+smallNum = i; 
+largeNum = num / i; 
+
+if (factors.length == 0) {
+factors = i; 
+} else {
+factors += ', ' + smallNum;
+}
+
+if (smallNum != largeNum) {
+factors += ', ' + largeNum; 
+}
+}
+}
+
+console.log(factors + '.');
+}
