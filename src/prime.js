a840887f957b843e40fbcc41629bb94dddd00810
jordan-j23 <jordanejenk@gmail.com>
2020-02-02 08:56:24 -0500

    Update prime.js

--- src/prime.js
+++ src/prime.js
@@ -1,1 +1,24 @@
 const readlineSync = require("readline-sync");
+
+let value = -1;
+let isPrimeNumber = true;
+
+while (value <= 0 ) {
+value = readlineSync.question("Non-negative integer: ");
+
+let num = parseInt( value );
+
+for( var i = 2; i < num; i++ ) {
+    if( num % i == 0) {
+      isPrimeNumber = false;
+    break;
+    }
+  }
+
+}
+
+if( isPrimeNumber) {
+console.log("\nPrime.");
+} else {
+console.log("\nNot Prime.");
+}
