893bf7cea9ba2ff34455288537b9cfcad2a1f883
jordan-j23 <jordanejenk@gmail.com>
2020-02-02 08:54:43 -0500

    Update average.js

--- src/average.js
+++ src/average.js
@@ -1,22 +1,20 @@
 const readlineSync = require("readline-sync");
 
-const INT_MIN = 0;
-const INT_MAX = Number.MAX_SAFE_INTEGER;
-let total = 0;
-let number = 0;
-let divisor = 0;
+let count = 0; 
+let sum = 0; 
+let value = 0;
 
-console.log();
+while (value >= 0) {
+value = readlineSync.question("Non-negative integer: ");
 
-do{
-  integer = Number(readlineSync.question("Non-negative integer: "));
-  if (number >= 0 && integer < Number.MAX_SAFE_INTEGER && !Number.isNaN(number) && integer % 1 == 0 && Number.isInteger(number)) {
-    total = total + number;
-    divisor = divisor + 1;
-  }
-while ((number >= 0) || Number.isNaN(number) || divisor == 0)
+num = parseInt ( value ) ;
+
+if( num >= 0) {
+count++;
+sum += num;
 }
-let avg = total / divisor;
-avg = avg.toLocaleString("en");
+}
 
-console.log("\n" + avg + ".")
+avg = sum / count;
+avg = avg.toLocaleString(undefined, {minimumFractionDigits:3} );
+console.log("\n" + avg + '.');
